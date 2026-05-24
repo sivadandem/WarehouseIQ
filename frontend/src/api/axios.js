bdc1bb466922ecf4a024acddb5d07cbdc88e4356
sivadandem <sivadandem7@gmail.com>
2026-05-24 17:03:01 +0530

    configure production api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('wiq_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
@@ -18,6 +24,7 @@ api.interceptors.response.use(
       localStorage.removeItem('wiq_token');
       window.location.href = '/login';
     }
+
     return Promise.reject(err);
   }
 );
